11c4eeb62a5c8bf0f2fd9957e140b0bfeeb82553
karina <123593292+Kuksito@users.noreply.github.com.>
2025-02-22 14:25:46 +0300

    Add logic to grow, remove, has, get, clear methods

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 export class HashMap {
-  loadFactor = 0.0625;
+  loadFactor = 0.75;
   capacity = 16;
-  buckets = Array.from({ length: 16 }, () => []);
-  bucketsCopy = Array.from({ length: 32 }, () => []);
+  buckets = Array.from({ length: this.capacity }, () => []);
   count = 0;
+
+  loadLevel() {
+    const level = this.count / this.capacity;
+    return level;
+  }
 
   hash(key) {
     let hashCode = 0;
     const primeNumber = 31;
 
     for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity; //or capacity = 16;??
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
     return hashCode;
   }
@@ -31,30 +35,42 @@ export class HashMap {
         obj.value = value;
         this.buckets.splice(index, 1, obj);
       } else {
-        // throw new Error("bucket is not empty");
         console.log("Error! bucket is not empty");
       }
     }
-    console.log(index);
-    return console.log(this.buckets);
+
+    if (this.loadLevel > this.loadFactor) {
+      this.grow;
+    } else {
+      console.log(false);
+    }
+
+    console.log(this.buckets);
   }
 
   get(key) {
-    if (!key) return null;
-    return key.value;
+    const obj = this.buckets.find((bucket) => bucket.key === key);
+    if (obj == undefined) return console.log(null);
+    else console.log(obj.value);
   }
 
   has(key) {
-    if (key) return true;
-    else return false;
+    const obj = this.buckets.find((bucket) => bucket.key === key);
+    if (obj == undefined) return console.log(false);
+    else console.log(true);
   }
 
   remove(key) {
-    if (key) {
-      //remove entry w the key n return true
+    const obj = this.buckets.find((bucket) => bucket.key === key);
+    const index = this.buckets.indexOf(obj);
+    if (index === -1) {
+      console.log(false);
     } else {
-      //return false
+      this.buckets[index] = [];
+      this.count--;
+      console.log(true);
     }
+    console.log(this.buckets);
   }
 
   length() {
@@ -98,8 +114,18 @@ export class HashMap {
     console.log(filtered);
   }
 
-  //implement near the end with some methods?? grow buckets ->
-  //double capacity when hashmap reaches loadFactor
-  //if (array.length > capacity * loadFactor)
-  //{ let newArray = []; newArray.copy(array); newArray.length = array.length*2}
+  grow() {
+    let bucketsCopy = this.buckets;
+    this.capacity *= 2;
+    this.buckets = new Array(this.capacity);
+    this.count = 0;
+
+    for (let bucket of bucketsCopy) {
+      if (bucket) {
+        for (let [key, value] of bucket) {
+          this.set(key, value);
+        }
+      }
+    }
+  }
 }
